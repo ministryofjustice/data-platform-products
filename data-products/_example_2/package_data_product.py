9c062e17991e40fba688e28071820cd4c9a2a7ba
Murdo <109604278+murdo-moj@users.noreply.github.com>
2023-04-17 09:29:32 +0100

    Dplab 000 fix file upload pathing (#20)
    * Fixed file pathing
    
    * Renamed workflow
    
    * Commit changes made by code formatters
    
    * Update .github/workflows/data-product-example-2-upload.yaml
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- data-products/_example_2/package_data_product.py
+++ data-products/_example_2/package_data_product.py
@@ -1,10 +1,24 @@
 import zipfile
+import logging
 import glob
+import os
+
+logging.getLogger().setLevel(logging.INFO)
+file_directory = os.path.dirname(os.path.abspath(__file__))
 
 
 def zip_data_product(folder: str):
-    with zipfile.ZipFile(f"data_product{folder}.zip", "w") as f:
-        for file in glob.glob(f"./{folder}/application/*.py") + glob.glob(f"./{folder}/requirements.txt"):
+    with zipfile.ZipFile(
+        os.path.join(file_directory, f"data_product{folder}.zip"), "w"
+    ) as f:
+        data_product_directory = os.path.join(file_directory, folder)
+        for file in (
+            glob.glob(os.path.join(
+                data_product_directory, "application", "*.py"))
+            + glob.glob(os.path.join(data_product_directory, "requirements.txt"))
+            + glob.glob(os.path.join(data_product_directory, "metadata", "*"))
+        ):
+            logging.info(file)
             f.write(file)
 
 
