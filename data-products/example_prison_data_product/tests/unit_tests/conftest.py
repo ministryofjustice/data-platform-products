f70da7ebcdb30bd22ece85f4e877b2136a4ddd39
LavMatt <mattlaverty@gmail.com>
2023-05-03 13:44:03 +0100

    tidy up comments

--- data-products/example_prison_data_product/tests/unit_tests/conftest.py
+++ data-products/example_prison_data_product/tests/unit_tests/conftest.py
@@ -7,14 +7,6 @@ from moto import mock_s3
 aws_region = "eu-west-1"
 
 
-# @pytest.fixture(scope="function")
-# def aws_credentials():
-#     """Mocked AWS Credentials for moto."""
-#     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-#     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-#     os.environ["AWS_SECURITY_TOKEN"] = "testing"
-#     os.environ["AWS_SESSION_TOKEN"] = "testing"
-#     os.environ["AWS_DEFAULT_REGION"] = aws_region
 @pytest.fixture(scope="module", autouse=True)
 def tests_env_setup_and_teardown():
 
@@ -33,9 +25,9 @@ def tests_env_setup_and_teardown():
     os.environ.update(TEMP_ENV_VARS)
 
     yield
-    # # Will be executed after the last test
-    # os.environ.clear()
-    # os.environ.update(old_environ)
+    # Will be executed after the last test
+    os.environ.clear()
+    os.environ.update(old_environ)
 
 
 @pytest.fixture(scope="function")
@@ -43,10 +35,3 @@ def s3_mock_client():
     with mock_s3():
         conn = boto3.client("s3", region_name=aws_region)
         yield conn
-
-
-# @pytest.fixture(scope="function")
-# def s3_mock_resource(aws_credentials):
-#     with mock_s3():
-#         conn = boto3.resource("s3", region_name=aws_region)
-#         yield conn
