249faae526678790be9ab1e5d9bbbedb2712d312
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2023-05-30 16:11:26 +0100

    Abstracted test data to csv

--- data-products/example_prison_data_product/tests/unit_tests/test_transform.py
+++ data-products/example_prison_data_product/tests/unit_tests/test_transform.py
@@ -15,67 +15,54 @@ def bucket_name():
 def s3_test_bucket(s3_mock_client, bucket_name):
     s3_mock_client.create_bucket(
         Bucket=bucket_name,
-        CreateBucketConfiguration={"LocationConstraint": "eu-west-1"}
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-1"},
     )
     yield
 
 
-data1 = {"Date": ["Q3", "Q3", "Q3", "Q3"],
-         "Outcomes": ["Dismissed", "Dismissed", "Dismissed", "Dismissed"],
-         "Predominant function of establishment": ["Category A (High Security)", "Category B", "Category C",
-                                                   "Local"],
-         "Establishment": ["Belmarsh", "Belmarsh", "Frankland", "Frankland"],
-         "Adjudicator": ["Governor", "Governor", "Governor", "Governor"],
-         "Sex": ["M", "M", "M", "M"],
-         "Age group": ["25 - 29", "30 - 39", "21 - 24", "30 - 39"],
-         "Ethnicity": ["b Black/ African/ Caribbean/ Black British", "e White",
-                       "c Mixed/ Multiple ethnic groups", "d Other ethnic group"],
-         "Religion": ["b Muslim", "a Christian", "i No religion", "b Muslim"],
-         "Offence": ["Disobedience/disrespect", "Disobedience/disrespect", "Unauthorised transactions",
-                     "Unauthorised transactions"],
-         "Detailed offence": ["Disobeys any lawful order", "Disobeys any lawful order", "Drug related offence",
-                              "Drug related offence"],
-         "Count": [1, 1, 1, 1]
-         }
+data_product_directory = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "..", "data"
+)
+
+sample_input = pd.read_csv(os.path.join(data_product_directory, "sample_input.csv"))
 
 
-table_dict = {'product_for_test': ['adj_example_1']}
+table_dict = {"product_for_test": ["adj_example_1"]}
 
 
-@patch('application.transform.get_data', return_value=pd.DataFrame.from_dict(data1))
-@patch('application.transform.get_tables', return_value=table_dict)
+@patch(
+    "application.transform.get_data", return_value=pd.DataFrame.from_dict(sample_input)
+)
+@patch("application.transform.get_tables", return_value=table_dict)
 def test_generate_report(mocked_data, mocked_tables):
-    expected_data = {"Establishment": ["Belmarsh", "Belmarsh", "Frankland", "Frankland"],
-                     "Religion": ["a Christian", "b Muslim", "b Muslim", "i No religion"],
-                     "Offence": ["Disobedience/disrespect", "Disobedience/disrespect", "Unauthorised transactions",
-                                 "Unauthorised transactions"],
-                     "Count": [1, 1, 1, 1]}
-    expected = pd.DataFrame.from_dict(expected_data)
+    data_product_directory = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "..", "data"
+    )
+    sample_output = pd.read_csv(
+        os.path.join(data_product_directory, "sample_output.csv")
+    )
     pd.testing.assert_frame_equal(
-        generate_report(
-            'fake_bucket',
-            'product_for_test/test_table1/data.csv'
-        )['product_for_test']['adj_example_1'],
-        expected
+        generate_report("fake_bucket", "product_for_test/test_table1/data.csv")[
+            "product_for_test"
+        ]["adj_example_1"],
+        sample_output,
     )
 
 
 def test_get_tables(s3_mock_client, s3_test_bucket):
     s3_mock_client.upload_file(
         os.path.join(
-            Path(__file__).parent.absolute(),
-            "test_metadata",
-            "02-data-dictionary.yaml"
+            Path(__file__).parent.absolute(), "test_metadata", "02-data-dictionary.yaml"
         ),
-        'product-test-bucket',
-        "code/product_for_test/extracted/metadata/02-data-dictionary.yml"
+        "product-test-bucket",
+        "code/product_for_test/extracted/metadata/02-data-dictionary.yml",
     )
 
-    expected_tables = {'product_for_test': ['test_table1']}
+    expected_tables = {"product_for_test": ["test_table1"]}
 
     tables = get_tables(
-        bucket='product-test-bucket',
-        key='raw_data/product_for_test/test_table1/data.csv',
-        source_data='test1'
+        bucket="product-test-bucket",
+        key="raw_data/product_for_test/test_table1/data.csv",
+        source_data="test1",
     )
     assert tables == expected_tables
